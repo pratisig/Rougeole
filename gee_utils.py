bbf56aac5a3b4404c19b101cd67987c2a92224b5
pratisig <37810753+pratisig@users.noreply.github.com>
2026-01-28 17:18:11 +0000

    Update gee_utils.py

--- gee_utils.py
+++ gee_utils.py
@@ -1,4 +1,28 @@
 # gee_utils.py
+def worldpop_by_age(ee_fc):
+    dataset = ee.ImageCollection("WorldPop/GP/100m/pop_age_sex")
+
+    age_bands = {
+        "under1": ["f_00", "m_00"],
+        "1_4": ["f_01", "m_01"],
+        "5_9": ["f_05", "m_05"]
+    }
+
+    results = {}
+
+    for group, bands in age_bands.items():
+        img = dataset.select(bands).sum()
+
+        stats = img.reduceRegions(
+            collection=ee_fc,
+            reducer=ee.Reducer.mean(),
+            scale=100
+        )
+
+        results[group] = stats
+
+    return results
+# gee_utils.py
 def urban_ratio(ee_fc):
     ghsl = ee.Image("JRC/GHSL/P2023A/GHS_SMOD_V2-0")
 

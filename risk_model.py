4ea8e59f4929263c3a0a91cec73be79669cbc1cb
pratisig <37810753+pratisig@users.noreply.github.com>
2026-01-28 17:19:10 +0000

    Delete risk_model.py

--- risk_model.py
+++
@@ -1,5 +0,0 @@
-# risk_model.py
-def predict_cases(ts, weeks=12):
-    trend = np.polyfit(range(len(ts)), ts, 1)
-    future = [trend[0]*i + trend[1] for i in range(len(ts), len(ts)+weeks)]
-    return future
